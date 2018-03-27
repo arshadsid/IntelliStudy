93fa6e525b2a2361c7ee173857d0752d8aa0ff38
Gautam Yadav <gautamyadav@iiitdmj.ac.in>
2018-03-27 21:32:48 +0530

    Changes

--- epsilon/urls.py
+++ epsilon/urls.py
@@ -3,4 +3,15 @@ from . import views
 
 
 urlpatterns = [
-]
+    url(r'^$', views.index, name='index'),
+    url(r'^about', views.about, name='about'),
+    url(r'^mentor', views.mentor, name='mentor'),
+    url(r'^student', views.student, name='student'),
+    url(r'^course', views.course, name='course'),
+    url(r'^quiz', views.quiz, name='quiz'),
+    url(r'^dashboard', views.dashboard, name='dashboard'),
+    url(r'^profile', views.profile, name='profile'),
+    url(r'^mycourses', views.mycourses, name='mycourses'),
+    url(r'^study', views.study, name='study'),
+    url(r'^group', views.group, name='group'),
+    ]
--- epsilon/views.py
+++ epsilon/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    context = {}
+    return render(request, "epsilon/index.html", context)
+
+
+def student(request):
+    context = {}
+    return render(request, "epsilon/loginstudent.html", context)
+
+
+def mentor(request):
+    context = {}
+    return render(request, "epsilon/mentorlogin.html", context)
+
+
+def dashboard(request):
+    context = {}
+    return render(request, "epsilon/student_dashboard.html", context)
+
+
+def course(request):
+    context = {}
+    return render(request, "epsilon/coursemain.html", context)
+
+
+def quiz(request):
+    context = {}
+    return render(request, "epsilon/quiz.html", context)
+
+
+def mycourses(request):
+    context = {}
+    return render(request, "epsilon/mycourses.html", context)
+
+
+def profile(request):
+    context = {}
+    return render(request, "epsilon/profile.html", context)
+
+
+def study(request):
+    context = {}
+    return render(request, "epsilon/coursestudy.html", context)
+
+
+def group(request):
+    context = {}
+    return render(request, "epsilon/coursegroup.html", context)
+
+
+def about(request):
+    return render(request, 'epsilon/about.html')
