be0c03672a52a47a4263b5646887e1d26a0ff089
Arshad Siddiqui <arshad.sid97@gmail.com>
2018-03-31 03:55:45 +0530

    Recommender

--- epsilon/views.py
+++ epsilon/views.py
@@ -11,6 +11,8 @@ from .models import (Course, Enroll, Student, Mentor, Question, ExtraInfo, Conte
                      File, Option, Contain, Group, Career, Has)
 
 
+
+
 def auth(request):
     username = request.POST.get('username', '')
     password = request.POST.get('password', '')
@@ -340,3 +342,238 @@ def editcourse(request):
 def loggedout(request):
     logout(request)
     return redirect('/epsilon')
+
+
+''' RECOMMENDER '''
+
+# for recommender
+import numpy as np
+import scipy.stats
+import scipy.spatial
+from sklearn.cross_validation import KFold
+import random
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+import math
+import warnings
+import sys
+
+
+def recommender_related(rec_for):
+    users = 27  # TODO: Change this to total number of students in the db
+    items = 19  # TODO: Change this to total number of courses in the db
+    recommend_data = readingFile("C:\\Users\\arsha\\Documents\\GitHub\\IntelliStudy\\epsilon\\grades.csv") # TODO: Change this address get grades from scores and store into a csv and avg based on courses
+    high1, high2, low1, low2 = predictRating(recommend_data, users, items, rec_for)
+    # highs are easiest for him to score high
+    # lows will be hard for him to score high
+    return HttpResponse("Done Recommendation\n High:" + str(high1) + " " + str(high2) + "\n LOW: " + str(low1) + " " + str(low2))
+
+def readingFile(filename):
+    f = open(filename,"r")
+    data = []
+    for row in f:
+        r = row.split(',')
+        e = [int(r[0]), int(r[1]), int(r[2])]
+        data.append(e)
+    return data
+
+def predictRating(recommend_data, users, items, rec_for):
+
+    M, sim_user = crossValidation(recommend_data, users, items)
+    pred_low1 = 10
+    pred_low2 = 10
+    pred_high1 = 0
+    pred_high2 = 0
+    f = open("C:\\Users\\arsha\\Documents\\GitHub\\IntelliStudy\\epsilon\\toBeGraded.csv","r")  # TODO: Change this address to the courses not enrolled
+    #f = open(sys.argv[2],"r")
+    toBeRated = {"user":[], "item":[]}
+    for row in f:
+        r = row.split(',')
+        toBeRated["item"].append(int(r[1]))
+        toBeRated["user"].append(rec_for)
+
+    f.close()
+
+    pred_rate = []
+
+    #fw = open('result1.csv','w')
+    fw_w = open('C:\\Users\\arsha\\Documents\\GitHub\\IntelliStudy\\epsilon\\result1.csv','w')  # TODO: Change this to return the results
+
+    l = len(toBeRated["user"])
+    for e in range(l):
+        user = toBeRated["user"][e]
+        item = toBeRated["item"][e]
+
+        pred = 5.0
+
+        #user-based
+        if np.count_nonzero(M[user-1]):
+            sim = sim_user[user-1]
+            ind = (M[:,item-1] > 0)
+            #ind[user-1] = False
+            normal = np.sum(np.absolute(sim[ind]))
+            if normal > 0:
+                pred = np.dot(sim,M[:,item-1])/normal
+
+        if pred < 0:
+            pred = 0
+
+        if pred > 10:
+            pred = 10
+
+        pred_rate.append(pred)
+
+        if(pred<pred_low1):
+            pred_low1=pred
+            low1 = item
+        elif (pred<pred_low2):
+            pred_low2=pred
+            low2 = item
+
+        if (pred > pred_high1):
+            pred_high1 = pred
+            high1 = item
+        elif (pred > pred_high2):
+            pred_high2 = pred
+            high2 = item
+
+        print (str(user) + "," + str(item) + "," + str(pred))
+        #fw.write(str(user) + "," + str(item) + "," + str(pred) + "\n")
+        fw_w.write(str(item) + "," + str(pred) + "\n")                      #   this is how you make csv
+
+    #fw.close()
+    fw_w.close()
+    return high1, high2, low1, low2
+
+def crossValidation(data, users, items):
+    k_fold = KFold(n=len(data), n_folds=10)
+
+    Mat = np.zeros((users,items))
+    for e in data:
+        Mat[e[0]-1][e[1]-1] = e[2]
+
+    sim_user_cosine, sim_user_jaccard, sim_user_pearson = similarity_user(Mat, users, items)
+
+    rmse_cosine = []
+    rmse_jaccard = []
+    rmse_pearson = []
+
+    for train_indices, test_indices in k_fold:
+        train = [data[i] for i in train_indices]
+        test = [data[i] for i in test_indices]
+
+        M = np.zeros((users,items))
+
+        for e in train:
+            M[e[0]-1][e[1]-1] = e[2]
+
+        true_rate = []
+        pred_rate_cosine = []
+        pred_rate_jaccard = []
+        pred_rate_pearson = []
+
+        for e in test:
+            user = e[0]
+            item = e[1]
+            true_rate.append(e[2])
+
+            pred_cosine = 5.0
+            pred_jaccard = 5.0
+            pred_pearson = 5.0
+
+            #user-based
+            if np.count_nonzero(M[user-1]):
+                sim_cosine = sim_user_cosine[user-1]
+                sim_jaccard = sim_user_jaccard[user-1]
+                sim_pearson = sim_user_pearson[user-1]
+                ind = (M[:,item-1] > 0)
+                #ind[user-1] = False
+                normal_cosine = np.sum(np.absolute(sim_cosine[ind]))
+                normal_jaccard = np.sum(np.absolute(sim_jaccard[ind]))
+                normal_pearson = np.sum(np.absolute(sim_pearson[ind]))
+                if normal_cosine > 0:
+                    pred_cosine = np.dot(sim_cosine,M[:,item-1])/normal_cosine
+
+                if normal_jaccard > 0:
+                    pred_jaccard = np.dot(sim_jaccard,M[:,item-1])/normal_jaccard
+
+                if normal_pearson > 0:
+                    pred_pearson = np.dot(sim_pearson,M[:,item-1])/normal_pearson
+
+            if pred_cosine < 0:
+                pred_cosine = 0
+
+            if pred_cosine > 10:
+                pred_cosine = 10
+
+            if pred_jaccard < 0:
+                pred_jaccard = 0
+
+            if pred_jaccard > 10:
+                pred_jaccard = 10
+
+            if pred_pearson < 0:
+                pred_pearson = 0
+
+            if pred_pearson > 10:
+                pred_pearson = 10
+
+            print (str(user) + "\t" + str(item) + "\t" + str(e[2]) + "\t" + str(pred_cosine) + "\t" + str(pred_jaccard) + "\t" + str(pred_pearson))
+            pred_rate_cosine.append(pred_cosine)
+            pred_rate_jaccard.append(pred_jaccard)
+            pred_rate_pearson.append(pred_pearson)
+
+        rmse_cosine.append(sqrt(mean_squared_error(true_rate, pred_rate_cosine)))
+        rmse_jaccard.append(sqrt(mean_squared_error(true_rate, pred_rate_jaccard)))
+        rmse_pearson.append(sqrt(mean_squared_error(true_rate, pred_rate_pearson)))
+
+        print (str(sqrt(mean_squared_error(true_rate, pred_rate_cosine))) + "\t" + str(sqrt(mean_squared_error(true_rate, pred_rate_jaccard))) + "\t" + str(sqrt(mean_squared_error(true_rate, pred_rate_pearson))))
+        #raw_input()
+
+    #print sum(rms) / float(len(rms))
+    rmse_cosine = sum(rmse_cosine) / float(len(rmse_cosine))
+    rmse_pearson = sum(rmse_pearson) / float(len(rmse_pearson))
+    rmse_jaccard = sum(rmse_jaccard) / float(len(rmse_jaccard))
+
+    print (str(rmse_cosine) + "\t" + str(rmse_jaccard) + "\t" + str(rmse_pearson))
+
+    f_rmse = open("rmse_user.txt","w")
+    f_rmse.write(str(rmse_cosine) + "\t" + str(rmse_jaccard) + "\t" + str(rmse_pearson) + "\n")
+
+    rmse = [rmse_cosine, rmse_jaccard, rmse_pearson]
+    req_sim = rmse.index(min(rmse))
+
+    print (req_sim)
+    f_rmse.write(str(req_sim))
+    f_rmse.close()
+
+    if req_sim == 0:
+        sim_mat_user = sim_user_cosine
+
+    if req_sim == 1:
+        sim_mat_user = sim_user_jaccard
+
+    if req_sim == 2:
+        sim_mat_user = sim_user_pearson
+
+    #predictRating(Mat, sim_mat_user)
+    return Mat, sim_mat_user
+
+def similarity_user(data, users, items):
+    user_similarity_cosine = np.zeros((users,users))
+    user_similarity_jaccard = np.zeros((users,users))
+    user_similarity_pearson = np.zeros((users,users))
+    for user1 in range(users):
+        print (user1)
+        for user2 in range(users):
+            if np.count_nonzero(data[user1]) and np.count_nonzero(data[user2]):
+                user_similarity_cosine[user1][user2] = 1-scipy.spatial.distance.cosine(data[user1],data[user2])
+                user_similarity_jaccard[user1][user2] = 1-scipy.spatial.distance.jaccard(data[user1],data[user2])
+                try:
+                    if not math.isnan(scipy.stats.pearsonr(data[user1],data[user2])[0]):
+                        user_similarity_pearson[user1][user2] = scipy.stats.pearsonr(data[user1],data[user2])[0]
+                    else:
+                        user_similarity_pearson[user1][user2] = 0
+                except:
+                    user_similarity_pearson[user1][user2] = 0
+    return user_similarity_cosine, user_similarity_jaccard, user_similarity_pearson
